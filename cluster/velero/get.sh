85c85150df5e70c2c6d4714d7437c77e179d9714
Carl Henrik Lunde <chlunde@ifi.uio.no>
2021-10-30 13:55:42 +0000

    velero: 1.7.0

--- cluster/velero/get.sh
+++ cluster/velero/get.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-curl --fail -SsLO https://github.com/vmware-tanzu/velero/releases/download/v1.7.0/velero-v1.7.0-linux-arm64.tar.gz
-tar -xvzf velero*.tar.gz
-mkdir -p ~/.local/bin
-mv velero-v*/velero ~/.local/bin
+if false; then
+	curl --fail -SsLO https://github.com/vmware-tanzu/velero/releases/download/v1.7.0/velero-v1.7.0-linux-arm64.tar.gz
+	tar -xvzf velero*.tar.gz
+	mkdir -p ~/.local/bin
+	mv velero-v*/velero ~/.local/bin
+fi
 
+kubectl get secret -n velero cloud-credentials -o jsonpath='{.data.cloud}' | base64 -d - > cloud-credentials
+velero install --dry-run -o yaml --provider aws --plugins velero/velero-plugin-for-aws:v1.3.0 --bucket k3s-backup  --backup-location-config region=minio,s3Url=http://192.168.1.150:9000,s3ForcePathStyle=true --snapshot-location-config region=minio,s3Url=http://192.168.1.150:9000,s3ForcePathStyle=true --use-restic --secret-file=cloud-credentials > velero.yaml
 # TODO: ...
-velero install --dry-run -o yaml --provider aws --plugins velero/velero-plugin-for-aws:v1.3.0 --bucket backups  --backup-location-config region=us-east-2 --snapshot-location-config region=us-east-2 --use-restic --no-secret > velero.yaml
+echo "Remove secret.."
