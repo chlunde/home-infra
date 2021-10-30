87d836b6210dd6c5c95ffab80c8dcebbd969dbcc
Carl Henrik Lunde <chlunde@ifi.uio.no>
2021-10-30 13:07:41 +0000

    external-dns: v0.10.1

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-curl -s https://toolkit.fluxcd.io/install.sh | sudo bash
+curl -s https://fluxcd.io/install.sh | sudo bash
 
 flux bootstrap github \
   --components=source-controller,kustomize-controller,helm-controller,notification-controller \
   --components-extra=image-reflector-controller,image-automation-controller \
-  --arch=arm64 --version=latest \
+  --version=latest \
   --owner='chlunde' \
   --repository='home-infra' \
   --path='./cluster' \
