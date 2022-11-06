92f8c038192cbac7f799616dc788bca4ccca40bb
Carl Henrik Lunde <chlunde@ifi.uio.no>
2022-11-06 20:09:11 +0100

    openebs-lite

--- cluster/openebs/get.sh
+++ cluster/openebs/get.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-curl --fail -SsLO https://openebs.github.io/charts/openebs-operator.yaml
+curl --fail -SsLO https://openebs.github.io/charts/openebs-operator-lite.yaml
+curl --fail -SsLO https://openebs.github.io/charts/openebs-lite-sc.yaml
