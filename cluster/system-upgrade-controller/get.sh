2746f9da29c55897eb29cb05afc35f0510f78922
Carl Henrik Lunde <chlunde@ifi.uio.no>
2022-11-06 19:49:06 +0000

    system-upgrade-controller

--- cluster/system-upgrade-controller/get.sh
+++ cluster/system-upgrade-controller/get.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --fail -SsLO https://github.com/rancher/system-upgrade-controller/releases/download/v0.8.0/system-upgrade-controller.yaml
+curl --fail -SsLO https://github.com/rancher/system-upgrade-controller/releases/download/v0.9.1/system-upgrade-controller.yaml
